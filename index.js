47fd83d393df350d26c275b71ef809284694b7f2
Gierek1205 <gierek1205@gmail.com>
2016-06-10 11:00:49 +0200

    Create Echo Bot

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 3000));
 
+
+
+// Server frontpage
+app.get('/', function (req, res) {
+    res.send(req.query['hub.challenge']);
+});
+
+// Facebook Webhook
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
 // generic function sending messages
 function sendMessage(recipientId, message) {
     request({
@@ -25,21 +40,6 @@ function sendMessage(recipientId, message) {
         }
     });
 };
-
-// Server frontpage
-app.get('/', function (req, res) {
-    res.send(req.query['hub.challenge']);
-});
-
-// Facebook Webhook
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
-});
-
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;

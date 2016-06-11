5067d1c8315150d93da603a27c475902be2e0455
Gierek1205 <gierek1205@gmail.com>
2016-06-11 16:04:24 +0200

    Create Echo Bot

--- index.js
+++ index.js
@@ -33,14 +33,21 @@ app.post('/', function (req, res) {
     res.sendStatus(200);
 });
 
-// handler receiving messages
-app.post('/', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
         }
-    }
-    res.sendStatus(200);
-});
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};

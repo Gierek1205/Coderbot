d1a0960612e5433b32940535de0121003dcefaa0
primosdace <marekmckl@gmail.com>
2016-06-18 11:11:38 +0200

    Test0

--- index.js
+++ index.js
@@ -25,48 +25,56 @@ app.get('/webhook', function(req, res) {
 
 // handler receiving messages---------------------------------------------------------------------------------------------------------
 app.post('/webhook', function(req, res) {
+	var events = req.body.entry[0].messaging;
+	var czas = new Date();
+	for (i = 0; i < events.length; i++) {
+		var event = events[i];
+		if (event.message && event.message.text) {
 
-			// our variables
-
-			var cities = ["warszawa", "cieszyn", "cisie", "zambrow", "bialystok"];
-
-			var events = req.body.entry[0].messaging;
-			for (i = 0; i < events.length; i++) {
-				var event = events[i];
-				if (event.message && event.message.text) {
-
-					if (event.message.text === "miasta") {
-						sendMessage(event.sender.id, {
-							text: "Wpisz (w nastepnej wiadomosci) nazwe dowolego miasta, w ktorym jest Coderdojo."
-						});
-					} else {
-						sendMessage(event.sender.id, {
-							text: "Wpisz (w nastepnej wiadomosci) nazwe dowolego miasta, w ktorym jest Coderdojoasdasd."
-						});
-					}
-				}
-				res.sendStatus(200);
-			};
-
-			// generic function sending messagesp-------------------------------------------------------------------------------------------------------
-			function sendMessage(recipientId, message) {
-				request({
-					url: 'https://graph.facebook.com/v2.6/me/messages',
-					qs: {
-						access_token: process.env.PAGE_ACCESS_TOKEN
-					},
-					method: 'POST',
-					json: {
-						recipient: {
-							id: recipientId
-						},
-						message: message,
-					}
-				}, function(error, response, body) {
-					if (error) {
-						console.log('Error sending message: ', error);
-					} else if (response.body.error) {
-						console.log('Error: ', response.body.error);
-					}
+			if (event.message.text === "!help") {
+				sendMessage(event.sender.id, {
+					text: "!pogoda, !time, itp.. itd..."
 				});
-			};
+			} else if (event.message.text === "!time") {
+				if (czas.getMinutes() < 10) {
+					"0" + czas.getMinutes();
+				}
+				sendMessage(event.sender.id, {
+					text: "Jest godzina " + (czas.getHours() + 2) + ":" + czas.getMinutes()
+				});
+
+			} else {
+				sendMessage(event.sender.id, {
+					text: "Maciek: " + event.message.text
+				});
+				sendMessage(event.sender.id, {
+					text: "Jesli nie wiesz co zrobic wpisz !help"
+				});
+			}
+		}
+	}
+	res.sendStatus(200);
+});
+
+// generic function sending messagesp-------------------------------------------------------------------------------------------------------
+function sendMessage(recipientId, message) {
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: process.env.PAGE_ACCESS_TOKEN
+		},
+		method: 'POST',
+		json: {
+			recipient: {
+				id: recipientId
+			},
+			message: message,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending message: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+	});
+};

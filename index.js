4f96897ce21536572b4db1073e953a3812111fc5
primosdace <marekmckl@gmail.com>
2016-06-18 18:35:45 +0200

    Zmienilem cos tam

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/webhook', function(req, res) {
 // handler receiving messages---------------------------------------------------------------------------------------------------------
 app.post('/webhook', function(req, res) {
 	var events = req.body.entry[0].messaging;
-	var cities = ["Warszawa", "Cieszyn", "Cisie", "Zambrow", "Bialystok", "Gdansk", "Gliwice"];
+	var cities = ["Warszawa", "Cieszyn", "Cisie", "Zambrów", "Białystok", "Gdańsk", "Gliwice"];
 	for (i = 0; i < events.length; i++) {
 		var event = events[i];
 		if (event.message && event.message.text) {

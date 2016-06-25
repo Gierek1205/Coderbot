2a4b17998f480f70cf986251965ac95ae76db4c9
pr1mosd4ce <marekmckl@gmail.com>
2016-06-25 12:14:17 +0200

    Adding edit

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/webhook', function(req, res) {
 // handler receiving messages---------------------------------------------------------------------------------------------------------
 app.post('/webhook', function(req, res) {
 	var events = req.body.entry[0].messaging;
-	var cities = ["Warszawa", "Cieszyn", "Cisie", "Zambrów", "Białystok", "Gdańsk", "Gliwice"];
+	var cities = ["Warszawa", "Cieszyn", "Cisie", "Zambrów", "Białystok", "Gdańsk", "Gliwice", "Poznań"];
 	for (i = 0; i < events.length; i++) {
 		var event = events[i];
 		if (event.message && event.message.text) {
